f45bc8dcd2c71a49347812b1bfc09c42f6a30eae
SpeedyPetey <pscholtens2001@gmail.com>
2025-02-20 09:56:56 -0800

    Updated notifications to work better and cleaner

--- Front_End/app/src/main/java/com/example/cmiyc/MainActivity.kt
+++ Front_End/app/src/main/java/com/example/cmiyc/MainActivity.kt
@@ -19,9 +19,6 @@ import com.google.firebase.firestore.firestore
 import com.google.firebase.Firebase
 import com.google.firebase.initialize
 import com.google.firebase.messaging.messaging
-import com.google.firebase.messaging.remoteMessage
-import kotlinx.coroutines.tasks.await
-import java.util.concurrent.atomic.AtomicInteger
 
 
 class MainActivity : ComponentActivity() {
@@ -34,7 +31,6 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         Firebase.initialize(this)
-        askNotificationPermission()
         logRegToken()
         setContent {
             CMIYCTheme {
@@ -46,121 +42,16 @@ class MainActivity : ComponentActivity() {
         }
     }
 
-
-//
-//    fun runtimeEnableAutoInit() {
-//        // [START fcm_runtime_enable_auto_init]
-//        Firebase.messaging.isAutoInitEnabled = true
-//        // [END fcm_runtime_enable_auto_init]
-//    }
-//
-//    fun deviceGroupUpstream() {
-//        // [START fcm_device_group_upstream]
-//        val to = "a_unique_key" // the notification key
-//        val msgId = AtomicInteger()
-//        Firebase.messaging.send(
-//            remoteMessage(to) {
-//                setMessageId(msgId.get().toString())
-//                addData("hello", "world")
-//            },
-//        )
-//        // [END fcm_device_group_upstream]
-//    }
-//
-//    fun sendUpstream() {
-//        val SENDER_ID = "YOUR_SENDER_ID"
-//        val messageId = 0 // Increment for each
-//        // [START fcm_send_upstream]
-//        val fm = Firebase.messaging
-//        fm.send(
-//            remoteMessage("$SENDER_ID@fcm.googleapis.com") {
-//                setMessageId(messageId.toString())
-//                addData("my_message", "Hello World")
-//                addData("my_action", "SAY_HELLO")
-//            },
-//        )
-//        // [END fcm_send_upstream]
-//    }
-//
-//    fun subscribeTopics() {
-//        // [START subscribe_topics]
-//        Firebase.messaging.subscribeToTopic("weather")
-//            .addOnCompleteListener { task ->
-//                var msg = "Subscribed"
-//                if (!task.isSuccessful) {
-//                    msg = "Subscribe failed"
-//                }
-//                Log.d(TAG, msg)
-//                Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-//            }
-//        // [END subscribe_topics]
-//    }
-//
-   fun logRegToken() {
-       // [START log_reg_token]
-       Firebase.messaging.getToken().addOnCompleteListener { task ->
-           if (!task.isSuccessful) {
-               Log.w(TAG, "Fetching FCM registration token failed", task.exception)
-               return@addOnCompleteListener
-           }
-
-           // Get new FCM registration token
-           val token = task.result
-
-           // Log and toast
-           val msg = "FCM Registration token: $token"
-           Log.d(TAG, msg)
-           Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
-       }
-       // [END log_reg_token]
-   }
-
-   // [START ask_post_notifications]
-   // Declare the launcher at the top of your Activity/Fragment:
-   private val requestPermissionLauncher = registerForActivityResult(
-       ActivityResultContracts.RequestPermission(),
-   ) { isGranted: Boolean ->
-       if (isGranted) {
-           // FCM SDK (and your app) can post notifications.
-       } else {
-           // TODO: Inform user that that your app will not show notifications.
-       }
-   }
-
-   private fun askNotificationPermission() {
-       // This is only necessary for API level >= 33 (TIRAMISU)
-       if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-           if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
-               PackageManager.PERMISSION_GRANTED
-           ) {
-               // FCM SDK (and your app) can post notifications.
-           } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-               // TODO: display an educational UI explaining to the user the features that will be enabled
-               //       by them granting the POST_NOTIFICATION permission. This UI should provide the user
-               //       "OK" and "No thanks" buttons. If the user selects "OK," directly request the permission.
-               //       If the user selects "No thanks," allow the user to continue without notifications.
-           } else {
-               // Directly ask for the permission
-               requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
-           }
-       }
-   }
-//    [END ask_post_notifications]
-//
-//    // [START get_store_token]
-//    private suspend fun getAndStoreRegToken(): String {
-//        val token = Firebase.messaging.token.await()
-//        // Add token and timestamp to Firestore for this user
-//        val deviceToken = hashMapOf(
-//            "token" to token,
-//            "timestamp" to FieldValue.serverTimestamp(),
-//        )
-//
-//        // Get user ID from Firebase Auth or your own server
-//        Firebase.firestore.collection("fcmTokens").document("myuserid")
-//            .set(deviceToken).await()
-//        return token
-//    }
-//    // [END get_store_token]
+    fun logRegToken() {
+        Firebase.messaging.getToken().addOnCompleteListener { task ->
+            if (!task.isSuccessful) {
+                return@addOnCompleteListener
+            }
+            val token = task.result
+            val msg = "FCM Registration token: $token"
+            Log.d(TAG, msg)
+            Toast.makeText(baseContext, msg, Toast.LENGTH_SHORT).show()
+        }
+    }
 
 }
--- Front_End/app/src/main/java/com/example/cmiyc/MyFirebaseMessagingService.kt
+++ Front_End/app/src/main/java/com/example/cmiyc/MyFirebaseMessagingService.kt
@@ -1,92 +1,39 @@
 package com.example.cmiyc
 
+import android.Manifest
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.media.RingtoneManager
 import android.os.Build
 import android.util.Log
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat.shouldShowRequestPermissionRationale
 import androidx.core.app.NotificationCompat
-import androidx.work.OneTimeWorkRequest
-import androidx.work.WorkManager
-import androidx.work.Worker
-import androidx.work.WorkerParameters
+import androidx.core.content.ContextCompat
+import com.example.cmiyc.MainActivity.Companion
+import com.google.firebase.Firebase
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
+import com.google.firebase.messaging.messaging
 
 class MyFirebaseMessagingService : FirebaseMessagingService() {
     companion object {
         private const val TAG = "MyFirebaseMsgService"
     }
 
-    // [START receive_message]
     override fun onMessageReceived(remoteMessage: RemoteMessage) {
-        // TODO(developer): Handle FCM messages here.
-        // Not getting messages here? See why this may be: https://goo.gl/39bRNJ
-        Log.d(TAG, "From: ${remoteMessage.from}")
-
-        // Check if message contains a data payload.
-        if (remoteMessage.data.isNotEmpty()) {
-            Log.d(TAG, "Message data payload: ${remoteMessage.data}")
-
-            // Check if data needs to be processed by long running job
-            if (needsToBeScheduled()) {
-                // For long-running tasks (10 seconds or more) use WorkManager.
-                scheduleJob()
-            } else {
-                // Handle message within 10 seconds
-                handleNow()
-            }
-        }
+        println("onMessageReceived")
 
         // Check if message contains a notification payload.
         remoteMessage.notification?.let {
             Log.d(TAG, "Message Notification Body: ${it.body}")
             sendNotification(it.body ?: "New message")
         }
-
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
-    }
-    // [END receive_message]
-
-    private fun needsToBeScheduled() = true
-
-    // [START on_new_token]
-    /**
-     * Called if the FCM registration token is updated. This may occur if the security of
-     * the previous token had been compromised. Note that this is called when the
-     * FCM registration token is initially generated so this is where you would retrieve the token.
-     */
-    override fun onNewToken(token: String) {
-        Log.d(TAG, "Refreshed token: $token")
-
-        // If you want to send messages to this application instance or
-        // manage this apps subscriptions on the server side, send the
-        // FCM registration token to your app server.
-        sendRegistrationToServer(token)
-    }
-    // [END on_new_token]
-
-    private fun scheduleJob() {
-        // [START dispatch_job]
-        val work = OneTimeWorkRequest.Builder(MyWorker::class.java)
-            .build()
-        WorkManager.getInstance(this)
-            .beginWith(work)
-            .enqueue()
-        // [END dispatch_job]
-    }
-
-    private fun handleNow() {
-        Log.d(TAG, "Short lived task is done.")
-    }
-
-    private fun sendRegistrationToServer(token: String?) {
-        // TODO: Implement this method to send token to your app server.
-        Log.d(TAG, "sendRegistrationTokenToServer($token)")
     }
 
     private fun sendNotification(messageBody: String) {
@@ -112,24 +59,8 @@ class MyFirebaseMessagingService : FirebaseMessagingService() {
 
         val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
-        // Since android Oreo notification channel is needed.
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                channelId,
-                "Channel human readable title",
-                NotificationManager.IMPORTANCE_DEFAULT,
-            )
-            notificationManager.createNotificationChannel(channel)
-        }
-
         val notificationId = 0
         notificationManager.notify(notificationId, notificationBuilder.build())
     }
 
-    internal class MyWorker(appContext: Context, workerParams: WorkerParameters) : Worker(appContext, workerParams) {
-        override fun doWork(): Result {
-            // TODO(developer): add long running task here.
-            return Result.success()
-        }
-    }
 }
