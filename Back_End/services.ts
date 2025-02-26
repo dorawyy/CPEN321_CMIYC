f9c4afe39348b7a426bc9bdd93ace750686b78d6
SpeedyPetey <pscholtens2001@gmail.com>
2025-02-26 14:41:44 -0800

    Updated services.ts:

--- Back_End/services.ts
+++ Back_End/services.ts
@@ -6,7 +6,7 @@ export const client = new MongoClient(process.env.DB_URI ?? "mongodb://localhost
 // Initialize Firebase Admin
 admin.initializeApp({
     credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID ?? "",
+        projectId: "cmiyc-eaaf9",
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? "",
         privateKey: (process.env.FIREBASE_PRIVATE_KEY ?? "").replace(/\\n/g, '\n'),
     }),
