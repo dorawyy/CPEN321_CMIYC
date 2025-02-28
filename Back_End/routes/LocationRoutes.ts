60828489919963d3bc54f380c26b8af1c156c162
Deepan  Chakravarthy <kdeepan240@gmail.com>
2025-02-28 09:46:51 -0800

    Reapply "update backend"
    This reverts commit e99bd7d2dc528ca1e361f7a7a63f719082126cbb.

--- Back_End/routes/LocationRoutes.ts
+++ Back_End/routes/LocationRoutes.ts
@@ -5,41 +5,11 @@ const controller = new LocationController();
 
 export const LocationRoutes = [
     {
-        method: "get",
-        route: "/location",
-        action: controller.getLocations,
-        validation: []
-    },
-    {
-        method: "post",
-        route: "/location/:userID",
-        action: controller.postUserLocation,
-        validation: [
-            body("userID").notEmpty().isString(),
-            body("location").notEmpty()
-        ]
-    },
-    {
-        method: "get",
-        route: "/location/:userID",
-        action: controller.getUserLocation,
-        validation: []
-    },
-    {
         method: "put",
         route: "/location/:userID",
         action: controller.updateUserLocation,
         validation: [
-            body("userID").notEmpty().isString(),
-            body("location").notEmpty()
-        ]
-    },
-    {
-        method: "delete",
-        route: "/location/:userID",
-        action: controller.deleteUserLocation,
-        validation: [
-            body("userID").notEmpty().isString()
+            body("currentLocation").notEmpty().isObject() // { latitude: number, longitude: number, timestamp: number }
         ]
     },
 ]
