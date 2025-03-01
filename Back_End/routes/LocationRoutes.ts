1295d7ce03bc589f4fb012e61b7b34608e74f14f
SpeedyPetey <pscholtens2001@gmail.com>
2025-02-28 16:34:59 -0800

    updated routes

--- Back_End/routes/LocationRoutes.ts
+++ Back_End/routes/LocationRoutes.ts
@@ -1,13 +1,14 @@
 import { LocationController } from "../controllers/LocationController";
 import { body } from "express-validator";
 
-const controller = new LocationController();
+const locationController = new LocationController();
 
 export const LocationRoutes = [
     {
         method: "put",
         route: "/location/:userID",
-        action: controller.updateUserLocation,
+        controller: locationController,
+        action: locationController.updateUserLocation,
         validation: [
             body("currentLocation").notEmpty().isObject() // { latitude: number, longitude: number, timestamp: number }
         ]
