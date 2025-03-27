47b693cc8cb44122c0cf14b2b44db751b25a5886
Deepan  Chakravarthy <kdeepan240@gmail.com>
2025-03-27 12:51:05 -0700

    Revert "fix map issue"
    This reverts commit e26d2f26ef4fdf6dd62a416e99a2fcee6c4d6317.

--- Front_End/app/src/main/java/com/example/cmiyc/ui/components/MapComponent.kt
+++ Front_End/app/src/main/java/com/example/cmiyc/ui/components/MapComponent.kt
@@ -53,17 +53,19 @@ fun MapComponent(
 ) {
     var lastUpdatedLocation by remember { mutableStateOf<Point?>(null) }
     val thresholdDistance = 25.0 // distance in meters
+
+    var isFirstUpdate = true
     val iconSizeDp = 64.dp
     val density = LocalDensity.current
     val iconSizePx = with(density) { iconSizeDp.roundToPx() }
 
     Box(modifier = modifier.fillMaxSize()) {
         MapboxMap(mapViewportState = mapViewportState, modifier = modifier) {
-            LocationUpdateHandler(mapViewportState = mapViewportState, lastUpdatedLocation = lastUpdatedLocation, thresholdDistance = thresholdDistance,
+            LocationUpdateHandler(mapViewportState = mapViewportState, lastUpdatedLocation = lastUpdatedLocation, thresholdDistance = thresholdDistance, isFirstUpdate = isFirstUpdate,
                 onLocationUpdated = { point ->
                     lastUpdatedLocation = point
                     UserRepository.locationManager.updateUserLocation(point)
-                },
+                }
             )
 
             val defaultUserIcon = rememberIconImage(
@@ -131,10 +133,9 @@ fun LocationUpdateHandler(
     mapViewportState: MapViewportState,
     lastUpdatedLocation: Point?,
     thresholdDistance: Double,
-    onLocationUpdated: (Point) -> Unit,
+    isFirstUpdate: Boolean,
+    onLocationUpdated: (Point) -> Unit
 ) {
-    var isFirstUpdate = true
-
     MapEffect(Unit) { mapView ->
         mapView.location.updateSettings {
             locationPuck = createDefault2DPuck(withBearing = true)
@@ -147,13 +148,12 @@ fun LocationUpdateHandler(
             val shouldUpdate = lastUpdatedLocation?.let {
                 calculateDistance(it, point) > thresholdDistance
             } ?: true
-            System.out.println("MapComponent: isFirstUpdate - " + isFirstUpdate)
+
             if (shouldUpdate) {
                 if (isFirstUpdate) {
                     mapViewportState.setCameraOptions(
                         cameraOptions { center(point); zoom(14.0) }
                     )
-                    isFirstUpdate = false
                 }
                 onLocationUpdated(point)
             }
